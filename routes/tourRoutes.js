21cc0f5ef6497044d20797f403a209e403e81cfc
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-07 15:08:30 +0530

    Removed checkBody function. This is now handled by tour Model

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const {
-	checkBody,
-	getAllTours,
-	createTour,
-	getTour,
-	updateTour,
-	deleteTour
-} = require('./../controllers/tourController');
+const { getAllTours, createTour, getTour, updateTour, deleteTour } = require('./../controllers/tourController');
 
 const router = express.Router();
 
-router.route('/').get(getAllTours).post(checkBody, createTour);
+router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
