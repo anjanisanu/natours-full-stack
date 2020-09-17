d04f42b663c23f746fd3d819f5b9ee68c8634524
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-17 07:54:17 +0530

    Added middleware in tour model. Slug also added when new tour is created.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,63 +1,91 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = mongoose.Schema({
-	name: {
-		type: String,
-		trim: true,
-		unique: [ true, 'A tour name must be unique' ],
-		required: [ true, 'A tour must have a name' ]
-	},
-	duration: {
-		type: Number,
-		required: [ true, 'A tour must have a duration' ]
-	},
-	maxGroupSize: {
-		type: Number,
-		required: [ true, 'A tour must have group size' ]
-	},
-	difficulty: {
-		type: String,
-		required: [ true, 'A tour must have a difficulty' ],
-		enum: {
-			values: [ 'easy', 'medium', 'difficult' ],
-			message: 'Difficulty must be either easy, medium, or difficult'
+const tourSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			trim: true,
+			unique: [ true, 'A tour name must be unique' ],
+			required: [ true, 'A tour must have a name' ]
+		},
+		slug: String,
+		duration: {
+			type: Number,
+			required: [ true, 'A tour must have a duration' ]
+		},
+		maxGroupSize: {
+			type: Number,
+			required: [ true, 'A tour must have group size' ]
+		},
+		difficulty: {
+			type: String,
+			required: [ true, 'A tour must have a difficulty' ],
+			enum: {
+				values: [ 'easy', 'medium', 'difficult' ],
+				message: 'Difficulty must be either easy, medium, or difficult'
+			}
+		},
+		ratingsAverage: {
+			type: Number,
+			default: 4.5
+		},
+		ratingsQuantity: {
+			type: Number,
+			default: 0
+		},
+		price: {
+			type: Number,
+			required: [ true, 'A tour must have a price' ]
+		},
+		priceDiscount: {
+			type: Number
+		},
+		summary: {
+			type: String,
+			trim: true,
+			required: [ true, 'A tour must have a summary' ]
+		},
+		description: {
+			type: String,
+			trim: true
+		},
+		imageCover: {
+			type: String,
+			required: [ true, 'A tour must have a cover image' ]
+		},
+		images: [ String ],
+		createdAt: {
+			type: Date,
+			default: Date.now(),
+			select: false
+		},
+		startDates: [ Date ],
+		secretTour: {
+			type: Boolean,
+			default: false
 		}
 	},
-	ratingsAverage: {
-		type: Number,
-		default: 4.5
-	},
-	ratingsQuantity: {
-		type: Number,
-		default: 0
-	},
-	price: {
-		type: Number,
-		required: [ true, 'A tour must have a price' ]
-	},
-	priceDiscount: {
-		type: Number
-	},
-	summary: {
-		type: String,
-		trim: true,
-		required: [ true, 'A tour must have a summary' ]
-	},
-	description: {
-		type: String,
-		trim: true
-	},
-	imageCover: {
-		type: String,
-		required: [ true, 'A tour must have a cover image' ]
-	},
-	images: [ String ],
-	createdAt: {
-		type: Date,
-		default: Date.now(),
-		select: false
-	},
-	startDates: [ Date ]
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true }
+	}
+);
+
+tourSchema.pre('save', function(next) {
+	this.slug = slugify(this.name, { lower: true });
+
+	next();
+});
+
+tourSchema.pre(/^find/, function(next) {
+	this.find({ secretTour: { $ne: true } });
+	next();
+});
+
+tourSchema.pre('aggregate', function(next) {
+	this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+	next();
 });
 
 module.exports = mongoose.model('Tour', tourSchema);
