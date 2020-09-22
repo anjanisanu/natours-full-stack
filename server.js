a1cde71e9d7f8860004fb4bf1626c58a6c6b91dd
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-22 20:03:19 +0530

    Added Error Handling for unCaught and unHandled Exception

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
-const connectDb = require('./db');
+process.on('uncaughtException', (err) => {
+	console.log(err.name, err.message);
+	console.log('Uncaught Exception. Shutting Down');
+	process.exit(1);
+});
 
+const connectDb = require('./db');
 const app = require('./app');
 
 connectDb();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
 	console.log(`App is running on PORT ${PORT}`);
+});
+
+process.on('unhandledRejection', (err) => {
+	console.log(err.name, err.message);
+	console.log('Unhandled Rejection. Shutting Down');
+	server.close(() => {
+		process.exit(1);
+	});
 });
