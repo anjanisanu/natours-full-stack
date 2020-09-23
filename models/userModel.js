9d948d972f3483953cac5eed39135d325410e2b3
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-23 12:18:11 +0530

    Added User Model

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [ true, 'Please tell us your name' ]
+	},
+	email: {
+		type: String,
+		required: [ true, 'Please provide your email' ],
+		unique: [ true, 'Email must be unique' ],
+		lowercase: true,
+		validate: [ validator.isEmail, 'Please enter a valid email' ]
+	},
+	photo: String,
+	password: {
+		type: String,
+		required: [ ture, 'Please provide a password' ],
+		minlength: 8
+	},
+	passwordConfirm: {
+		type: String,
+		required: [ true, 'Please confirm your password' ]
+	}
+});
+
+module.exports = mongoose.model('User', userSchema);
