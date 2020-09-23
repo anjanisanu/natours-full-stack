262cb0c3110c0a1bb1913d46eec2d415eea45896
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-23 18:03:23 +0530

    Added getAllUsers functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,17 @@
-exports.getAllUsers = (req, res) => {
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+	const users = await User.find();
+
 	res.status(200).json({
 		status: 'success',
+		results: users.length,
 		data: {
-			users: 'Displays all users'
+			users
 		}
 	});
-};
+});
 
 exports.getUser = (req, res) => {
 	res.status(200).json({
