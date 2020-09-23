da07d364ca505aa29e925aac75af239cdaf1cc6b
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-23 12:37:27 +0530

    Added signup route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const authController = require('./../controllers/authController');
 const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('./../controllers/userController');
 
 const router = express.Router();
 
+router.route('/signup').post(authController.signup);
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
