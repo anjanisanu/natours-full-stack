8100a67a48c20f01b0b52123b11ef527a6249903
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-26 23:50:46 +0530

    Added restriction in create, update, delete tour for normal users

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,11 +7,19 @@ const {
 	deleteTour,
 	aliasTopTours
 } = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
 router.route('/top-3-tours').get(aliasTopTours, getAllTours);
-router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router
+	.route('/')
+	.get(getAllTours)
+	.post(authController.protect, authController.restrictTo('admin', 'lead-guide'), createTour);
+router
+	.route('/:id')
+	.get(getTour)
+	.patch(authController.protect, authController.restrictTo('admin', 'lead-guide'), updateTour)
+	.delete(authController.protect, authController.restrictTo('admin'), deleteTour);
 
 module.exports = router;
