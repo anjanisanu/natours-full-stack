e52986ca3f6ead5ad63c0fc8897e51f8c732bcca
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-27 19:38:24 +0530

    Added Pre save hook to change passwordChangedAt property when password is changed

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,6 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const { resetPassword } = require('../controllers/authController');
 
 const userSchema = new mongoose.Schema({
 	name: {
@@ -51,6 +50,13 @@ userSchema.pre('save', async function(next) {
 
 	this.password = await bcrypt.hash(this.password, 12);
 	this.passwordConfirm = undefined;
+
+	next();
+});
+
+userSchema.pre('save', function(next) {
+	if (!this.isModified('password') || this.isNew) return next();
+	this.passwordChangedAt = Date.now() - 1000;
 
 	next();
 });
