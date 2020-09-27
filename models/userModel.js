92fd66683c6f5ef933eccd238370e943eb9ccdd4
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-27 17:38:20 +0530

    Added instance method for creating password reset token

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const { resetPassword } = require('../controllers/authController');
 
 const userSchema = new mongoose.Schema({
 	name: {
@@ -39,7 +41,9 @@ const userSchema = new mongoose.Schema({
 	passwordChangedAt: {
 		type: Date,
 		default: Date.now()
-	}
+	},
+	passwordResetToken: String,
+	passwordResetExpires: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -63,6 +67,15 @@ userSchema.methods.changedPasswordAfter = function(JWTTimeStamp) {
 	}
 
 	return false;
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+	const resetToken = crypto.randomBytes(32).toString('hex');
+
+	this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+	this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+	return resetToken;
 };
 
 module.exports = mongoose.model('User', userSchema);
