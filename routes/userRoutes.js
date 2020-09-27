b4c6cff72c762a8853fa1ef8ccfba12b862ba329
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-27 19:40:15 +0530

    Change http method from post to patch in resetPassword route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 router.route('/forgotPassword').post(authController.forgotPassword);
-router.route('/resetPassword').post(authController.resetPassword);
+router.route('/resetPassword/:token').patch(authController.resetPassword);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
