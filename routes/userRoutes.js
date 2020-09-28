8855091e1f254afe0803b791010d10323e018ada
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-28 19:30:43 +0530

    Added updateMe route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 
 const authController = require('./../controllers/authController');
-const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('./../controllers/userController');
+const {
+	getAllUsers,
+	createUser,
+	getUser,
+	updateUser,
+	updateMe,
+	deleteUser
+} = require('./../controllers/userController');
 
 const router = express.Router();
 
@@ -11,6 +18,7 @@ router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
 
 router.route('/updatePassword').patch(authController.protect, authController.updatePassword);
+router.patch('/updateMe', authController.protect, updateMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
