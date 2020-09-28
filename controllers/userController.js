5e8cf140410e08af5a64ffaf13c6599387e39a54
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-28 19:29:57 +0530

    Added updateMe functionality to update current user name and email

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+	const newObj = {};
+	Object.keys(obj).forEach((el) => {
+		if (allowedFields.includes(el)) newObj[el] = obj[el];
+	});
+	return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
 	const users = await User.find();
@@ -39,6 +48,23 @@ exports.updateUser = (req, res) => {
 		}
 	});
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+	// Create error if user post password data
+	if (req.body.password || req.body.passwordConfirm)
+		return next(new AppError('This route is not for updating password', 400));
+
+	// update user document
+	const filteredBody = filterObj(req.body, 'name', 'email');
+	const updatedData = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			user: updatedData
+		}
+	});
+});
 
 exports.deleteUser = (req, res) => {
 	res.status(204).json({
