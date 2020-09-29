b013603a0e5d00c20bdfb9261c7794069a7af442
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-29 16:47:34 +0530

    Added rate limiter to prevent too many requests from one IP

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -6,6 +7,15 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+
+//Global Middlewares
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many requests from this IP. Please try again after an hour'
+});
+
+app.use('/api', limiter);
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
