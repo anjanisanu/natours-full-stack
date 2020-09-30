a09dbe1a99d0e947abdce2b993c86817cb236c3f
Anjani Sanu <anjanikrsanu@gmail.com>
2020-09-30 20:04:03 +0530

    Populate users linked with tour while fetching tour details

--- models/tourModel.js
+++ models/tourModel.js
@@ -122,6 +122,15 @@ tourSchema.pre(/^find/, function(next) {
 	next();
 });
 
+tourSchema.pre(/^find/, function(next) {
+	this.populate({
+		path: 'guides',
+		select: '-__v -passwordChangedAt -email'
+	});
+
+	next();
+});
+
 tourSchema.pre('aggregate', function(next) {
 	this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 	next();
