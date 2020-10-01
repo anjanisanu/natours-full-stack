f225dd1dd5950d36244830f74351257d3ffdaf21
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-01 20:08:58 +0530

    Added route for creating a tour

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const authController = require('./../controllers/authController');
 const reviewController = require('./../controllers/reviewController');
 
 const router = express.Router();
 
-router.route('/').get(reviewController.getAllReviews);
+router
+	.route('/')
+	.get(reviewController.getAllReviews)
+	.post(authController.protect, authController.restrictTo('user', 'admin'), reviewController.createReview);
 
 module.exports = router;
