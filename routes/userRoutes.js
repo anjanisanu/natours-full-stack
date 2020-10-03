eed5f5c0a426d1d12686f4e30ff228ce262ff494
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-03 22:45:32 +0530

    Added authorization on some routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,10 +18,14 @@ router.route('/login').post(authController.login);
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
 
-router.route('/updatePassword').patch(authController.protect, authController.updatePassword);
-router.get('/me', authController.protect, getMe, getUser);
-router.patch('/updateMe', authController.protect, updateMe);
-router.delete('/deleteMe', authController.protect, deleteMe);
+router.use(authController.protect);
+
+router.route('/updatePassword').patch(authController.updatePassword);
+router.get('/me', getMe, getUser);
+router.patch('/updateMe', updateMe);
+router.delete('/deleteMe', deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router.route('/').get(getAllUsers);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
