74387363d10140c18117660026b8b4d9b5d595a7
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-05 23:11:55 +0530

    Changed the user populate options from name email to name photo

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -38,7 +38,7 @@ reviewSchema.index({ tour: 1, user: 1 }, { unique: 1 });
 reviewSchema.pre(/^find/, function(next) {
 	this.populate({
 		path: 'user',
-		select: 'name email'
+		select: 'name photo'
 	});
 	next();
 });
